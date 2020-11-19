87e3be8e32efafce5af8169bec9b83c001c5ab6b
Erik Frej Knudsen <erikfrejknudsen@h144.aau1x-2.klient.sydhavn.site.aau.dk>
2020-11-19 16:12:15 +0100

    test commit

--- Source/AdditiveComponent.cpp
+++ Source/AdditiveComponent.cpp
@@ -11,29 +11,56 @@
 #include <JuceHeader.h>
 #include "AdditiveComponent.h"
 
+using namespace juce;
+
 //==============================================================================
 AdditiveComponent::AdditiveComponent()
 {
     shiftSlider.setSliderStyle(juce::Slider::SliderStyle::RotaryHorizontalVerticalDrag);
     shiftSlider.setTextBoxStyle(juce::Slider::NoTextBox, true, 0, 0);
+    shiftSlider.setPopupDisplayEnabled(true, true, this);
+    shiftSlider.setTextValueSuffix (" Hz or whatever");
     shiftSlider.setRange(0.0f, 10.0f, 0.1f);
     shiftSlider.setValue(5.0f);
     addAndMakeVisible(shiftSlider);
     shiftSlider.setBounds(0, 0, 100, 100);
+
     
     stretchSlider.setSliderStyle(juce::Slider::SliderStyle::RotaryHorizontalVerticalDrag);
     stretchSlider.setTextBoxStyle(juce::Slider::NoTextBox, true, 0, 0);
+    stretchSlider.setPopupDisplayEnabled(true, true, this);
+    stretchSlider.setTextValueSuffix (" Hz or whatever");
     stretchSlider.setRange(0.0f, 10.0f, 0.1f);
     stretchSlider.setValue(5.0f);
     addAndMakeVisible(stretchSlider);
     stretchSlider.setBounds(0, 0, 100, 100);
     
+    
     ampSlider.setSliderStyle(juce::Slider::SliderStyle::RotaryHorizontalVerticalDrag);
     ampSlider.setTextBoxStyle(juce::Slider::NoTextBox, true, 0, 0);
+    ampSlider.setPopupDisplayEnabled(true, true, this);
+    ampSlider.setTextValueSuffix (" Hz or whatever");
     ampSlider.setRange(0.0f, 10.0f, 0.1f);
     ampSlider.setValue(5.0f);
     addAndMakeVisible(ampSlider);
     ampSlider.setBounds(0, 0, 100, 100);
+    
+    
+    addAndMakeVisible(shiftSliderLabel);
+    shiftSliderLabel.setText("Shift", juce::dontSendNotification);
+    shiftSliderLabel.setJustificationType(Justification::centred);
+    
+    addAndMakeVisible(stretchSliderLabel);
+    stretchSliderLabel.setText("Stretch", juce::dontSendNotification);
+    stretchSliderLabel.setJustificationType(Justification::centred);
+
+    
+    addAndMakeVisible(ampSliderLabel);
+    ampSliderLabel.setText("Amp", juce::dontSendNotification);
+    ampSliderLabel.setJustificationType(Justification::centred);
+    
+    addAndMakeVisible(moduleToggle);
+        
 }
 
 AdditiveComponent::~AdditiveComponent()
@@ -49,6 +76,8 @@ void AdditiveComponent::paint (juce::Graphics& g)
     g.setFont (14.0f);
     g.drawText ("Additive", getLocalBounds(),
                 juce::Justification::centred, true);   // draw some placeholder text
+
+    
 }
 
 void AdditiveComponent::resized()
@@ -74,8 +103,13 @@ void AdditiveComponent::resized()
     grid.items = {
         juce::GridItem (shiftSlider).withArea(6, 2, 9, 4),
         juce::GridItem (stretchSlider).withArea(6, 4, 9, 6),
-        juce::GridItem (ampSlider).withArea(6, 10, 9, 12)
+        juce::GridItem (ampSlider).withArea(6, 10, 9, 12),
+        juce::GridItem (shiftSliderLabel).withArea(8, 2, 11, 4),
+        juce::GridItem (stretchSliderLabel).withArea(8, 4, 11, 6),
+        juce::GridItem (ampSliderLabel).withArea(8, 10, 11, 12),
+        juce::GridItem (moduleToggle).withArea(1, 12, 1, 12)
     };
 
     grid.performLayout (getLocalBounds());
+    
 }
